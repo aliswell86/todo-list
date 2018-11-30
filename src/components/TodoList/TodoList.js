412fe249b582dda406d7d6505f7f13fde63bdb0d
kdk <aliswell86@naver.com>
2018-11-30 17:40:26 +0900

    11.30

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TodoItem from '../TodoItem';
+
+class TodoList extends React.Component {
+  render () {
+    const {todos} = this.props;
+    const todoList = todos.map(
+      todo => (
+        <TodoItem
+          key={todo.id}
+          done={todo.done}>
+          {todo.text}
+        </TodoItem>
+      )
+    )
+    return (
+      <div>
+        {todoList}
+      </div>
+    )
+  }
+}
+
+export default TodoList;
